9713626cad326a1c5af94f1f05c9547411f85fac
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-04-03 19:25:16 +0000

    fix: update blocking libraries for express 5

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,11 @@
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
 
 import { notFoundHandler } from './middlewares/notFound.middleware.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 import routes from './routes/index.js';
-
-dotenv.config();
 
 const app = express();
 
@@ -21,7 +17,6 @@ app.use(
     credentials: true,
   }),
 );
-app.use(mongoSanitize());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 
