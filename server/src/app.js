c734813a7d1078268beadbf10912bb9be979fc42
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-04-06 15:00:11 +0000

    fix(bug): resolve that JWT cors bug

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { notFoundHandler } from './middlewares/notFound.middleware.js';
@@ -18,8 +18,15 @@ const uploadDirectory = path.resolve(__dirname, '../uploads');
 // 	credentials: true,
 // };
 
-app.use(helmet());
-app.use(cors());
+// app.use(
+// 	helmet({
+// 		crossOriginOpenerPolicy: { policy: 'unsafe-none' },
+// 	}),
+// );
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
